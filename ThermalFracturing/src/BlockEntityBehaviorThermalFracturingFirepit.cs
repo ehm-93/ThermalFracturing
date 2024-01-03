91ba0eac522a17630a676ad42760ee01c51c6b69
Emmett Miller <ehm-93@users.noreply.github.com>
2024-01-02 18:33:11 -0800

    refactor: simplify BlockEntityBehaviorThermalFracturingFirepit

--- ThermalFracturing/src/BlockEntityBehaviorThermalFracturingFirepit.cs
+++ ThermalFracturing/src/BlockEntityBehaviorThermalFracturingFirepit.cs
@@ -7,8 +7,6 @@ namespace Ehm93.VintageStory.ThermalFracturing
 {
     class BlockEntityBehaviorThermalFracturingFirepit : BlockEntityBehavior
     {
-        private long tickRegistration = 0L;
-
         public BlockEntityBehaviorThermalFracturingFirepit(BlockEntityFirepit blockEntity) : base(blockEntity)
         {
         }
@@ -16,53 +14,27 @@ namespace Ehm93.VintageStory.ThermalFracturing
         public override void Initialize(ICoreAPI api, JsonObject properties)
         {
             base.Initialize(api, properties);
-
-            if (tickRegistration != 0L) {
-                Api.World.UnregisterGameTickListener(tickRegistration);
-            }
-            var ticker = new Ticker(this, Api.World, Pos);
-            tickRegistration = Api.World.RegisterGameTickListener(ticker.Tick, 1000);
+            Blockentity.RegisterGameTickListener(Tick, 1000);
         }
 
-        public override void OnBlockRemoved()
+        private void Tick(float _)
         {
-            base.OnBlockRemoved();
+            DoTick(Pos.NorthCopy());
+            DoTick(Pos.EastCopy());
+            DoTick(Pos.SouthCopy());
+            DoTick(Pos.WestCopy());
+            DoTick(Pos.UpCopy());
+            DoTick(Pos.DownCopy());
+        }
+
+        private void DoTick(BlockPos neighborPos)
+        {
+            var neighbor = Api.World.BlockAccessor.GetBlock(neighborPos);
+            if (!neighbor.HasBehavior<BlockBehaviorThermalFracturingTarget>()) return;
             
-            Api.World.UnregisterGameTickListener(tickRegistration);
-        }
-
-        private class Ticker
-        {
-            private readonly BlockEntityBehaviorThermalFracturingFirepit parent;
-            private readonly IWorldAccessor world;
-            private readonly BlockPos blockPos;
-
-            public Ticker(BlockEntityBehaviorThermalFracturingFirepit parent, IWorldAccessor world, BlockPos blockPos)
-            {
-                this.parent = parent;
-                this.world = world;
-                this.blockPos = blockPos.Copy();
-            }
-
-            public void Tick(float _)
-            {
-                DoTick(blockPos.NorthCopy());
-                DoTick(blockPos.EastCopy());
-                DoTick(blockPos.SouthCopy());
-                DoTick(blockPos.WestCopy());
-                DoTick(blockPos.UpCopy());
-                DoTick(blockPos.DownCopy());
-            }
-
-            public void DoTick(BlockPos neighborPos)
-            {
-                var neighbor = world.BlockAccessor.GetBlock(neighborPos);
-                if (!neighbor.HasBehavior<BlockBehaviorThermalFracturingTarget>()) return;
-                
-                var target = neighbor.GetBehavior<BlockBehaviorThermalFracturingTarget>();
-                var temperature = (parent.Blockentity as BlockEntityFirepit).furnaceTemperature;
-                target.CheckTransition(world, neighborPos, temperature);
-            }
+            var target = neighbor.GetBehavior<BlockBehaviorThermalFracturingTarget>();
+            var temperature = (Blockentity as BlockEntityFirepit).furnaceTemperature;
+            target.CheckTransition(Api.World, neighborPos, temperature);
         }
     }
 }
